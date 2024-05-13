6a67225dfaedf3fe0dacdeba362bdabc92175514
Omar <omar.piedrahitat@gmail.com>
2024-05-13 13:55:46 -0500

    Agrego enums al combo box

--- src/main/java/com/uq/taller_cola/Procesos/TransaccionController.java
+++ src/main/java/com/uq/taller_cola/Procesos/TransaccionController.java
@@ -1,12 +1,18 @@
 package com.uq.taller_cola.Procesos;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class TransaccionController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransaccionController implements Initializable {
 
     @FXML
     private Label LblCliente;
@@ -18,13 +24,15 @@ public class TransaccionController {
     private Label LblTipo;
 
     @FXML
-    private ComboBox<?> cmbTipo;
+    private ComboBox<TipoTransaccion> cmbTipo;
 
     @FXML
     private TextField txtCliente;
 
     @FXML
     private TextField txtDetalle;
+
+    private ObservableList<TipoTransaccion> observableTransaccion;
 
     @FXML
     void Cliente(ActionEvent event) {
@@ -39,6 +47,15 @@ public class TransaccionController {
     @FXML
     void tipoTransaccion(ActionEvent event) {
 
+
+
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        observableTransaccion = FXCollections.observableArrayList(TipoTransaccion.APERTURA_CUENTA,TipoTransaccion.CONSULTA_SALDO,TipoTransaccion.SOLICITUD_CREDITO);
+        cmbTipo.setItems(observableTransaccion);
+
+    }
 }
